fffcb3fed36ba139f65c136b4e45a7cc7b4bbf6e
tarunshrestha <tarunshrestha90@gmail.com>
2024-07-23 18:34:50 +0545

    Vue Basic setup complete with Vite

---
+++ Vue-Vite/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>About Us</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, tempore! Tempora saepe id eligendi rerum iusto eius cumque, reiciendis nihil expedita qui, delectus dignissimos vero perferendis consequatur voluptate adipisci iure.
+    
+  </p>
+  </template>
---
+++ Vue-Vite/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Home</h1>
+    <CounterButton />
+  
+  </template>
+
+
+
+<script setup>
+import CounterButton from '../components/CounterButton.vue'
+
+
+</script>
